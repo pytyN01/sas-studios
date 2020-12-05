3e23f8be0bacf1a4714952fc20e477e75be2cf57
Alexis V Resendiz <ajvladimir@gmail.com>
2020-12-04 16:45:31 -0800

    doneish

--- pages/contact.js
+++ pages/contact.js
@@ -70,7 +70,7 @@ export default function Home() {
               xs={12}
               sm={6}
               style={{
-                background: "url(/Tennessee.jpg) no-repeat center center fixed",
+                background: "url(/T.jpg) no-repeat center center fixed",
                 backgroundSize: "cover",
               }}
               className="center-text"
@@ -101,7 +101,7 @@ export default function Home() {
               xs={12}
               sm={6}
               style={{
-                background: "url(/Kentucky.jpg) no-repeat center center fixed",
+                background: "url(/K.jpg) no-repeat center center fixed",
                 backgroundSize: "cover",
               }}
               className="center-text"
@@ -125,7 +125,7 @@ export default function Home() {
             </Grid>
             <Grid item xs={12} sm={6}>
               <Iframe
-                url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3205.578024608965!2d-86.89030908486039!3d36.540173489993464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8864556e9147cadf%3A0xa4c7d529747b6eaa!2s210%20Charles%20Ralph%20Dr%2C%20Springfield%2C%20TN%2037172!5e0!3m2!1sen!2sus!4v1607120035266!5m2!1sen!2sus"
+                url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25434.201752583594!2d-85.9293050568692!3d37.16992990917649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886621b4a78bb443%3A0x391d89602eb7e652!2sHorse%20Cave%2C%20KY%2042749!5e0!3m2!1sen!2sus!4v1607128754476!5m2!1sen!2sus"
                 width="100%"
                 height="775px"
                 frameBorder="0"
@@ -141,7 +141,7 @@ export default function Home() {
           <Grid container spacing={0}>
             <Grid item xs={12} sm={6}>
               <Iframe
-                url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3205.578024608965!2d-86.89030908486039!3d36.540173489993464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8864556e9147cadf%3A0xa4c7d529747b6eaa!2s210%20Charles%20Ralph%20Dr%2C%20Springfield%2C%20TN%2037172!5e0!3m2!1sen!2sus!4v1607120035266!5m2!1sen!2sus"
+                url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.080230355274!2d-118.22390488492094!3d34.067457524375804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c6646c68be15%3A0xfb25f66d521d7960!2s501%20S%20Avenue%2017%2C%20Los%20Angeles%2C%20CA%2090031!5e0!3m2!1sen!2sus!4v1607128869748!5m2!1sen!2sus"
                 width="100%"
                 height="775px"
                 frameBorder="0"
